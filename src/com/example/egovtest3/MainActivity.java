58f08244fcd1ae335be829bd8eadb6d127a174d4
thkim <taehwan.kim@nhn.com>
2016-03-04 19:34:56 +0900

    initialize commit

---
+++ src/com/example/egovtest3/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.egovtest3;
+
+import kr.go.egovframework.hyb.deviceinfoapp.R;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class MainActivity extends ActionBarActivity {
+
+	private WebView mWebView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
